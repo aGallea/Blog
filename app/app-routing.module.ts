4f95829af52db09eaac72af9c34e57492acc6da9
gadgallea <gadgallea@gmail.com>
2017-01-08 14:27:46 +0200

    Update app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PostsListComponent }    from './posts/posts-list/posts-list.component';
+import { ProjectsPostsListComponent }    from './posts/projects-posts-list/projects-posts-list.component';
+import { ToolsPostsListComponent }    from './posts/tools-posts-list/tolls-posts-list.component';
 import { AboutComponent }    from './about/about.component';
 import { ContactComponent }    from './contact/contact.component';
 import { PostDetailsComponent }    from './posts/post-details/post-details.component';
@@ -9,8 +11,8 @@ import { PostDetailsComponent }    from './posts/post-details/post-details.compo
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: PostsListComponent },
-  { path: 'tools',  component: PostsListComponent },
-  { path: 'projects',  component: PostsListComponent },
+  { path: 'tools',  component: ToolsPostsListComponent },
+  { path: 'projects',  component: ProjectsPostsListComponent },
   { path: 'about',  component: AboutComponent },
   { path: 'postdetails/:id', component: PostDetailsComponent },
   { path: 'contact',  component: ContactComponent }
