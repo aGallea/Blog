624e31c694b7211096a0f903e6a00c2ffe416a12
gadgallea <gadgallea@gmail.com>
2017-01-08 09:07:51 +0200

    Update app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostsListComponent }    from './posts/posts-list/posts-list.component';
 import { AboutComponent }    from './about/about.component';
+import { PostDetailsComponent }    from './posts/post-details/post-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: PostsListComponent },
-  { path: 'about',  component: AboutComponent }
+  { path: 'about',  component: AboutComponent },
+  { path: 'postdetails/:id', component: PostDetailsComponent },
 ];
 
 @NgModule({
