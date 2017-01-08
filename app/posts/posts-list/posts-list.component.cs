4324242118dbc21cadfbadbaf8a8b2740982373e
gadgallea <gadgallea@gmail.com>
2017-01-08 08:59:03 +0200

    Create posts-list.component.cs

---
+++ app/posts/posts-list/posts-list.component.cs
@@ -0,0 +1,38 @@
+#fh5co-board .post {
+  margin: 10px 10px 20px 10px;
+  /*background: #fff;*/
+  /*-webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  -ms-border-radius: 4px;*/
+  border-radius: 4px;
+  overflow: hidden;
+  -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
+  -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
+}
+
+#fh5co-board .item img {
+  max-width: 100%;
+  -webkit-transition: 0.2s;
+  -o-transition: 0.2s;
+  transition: 0.2s;
+}
+
+.post  img{
+	border-radius: 4px 4px 0em 0em;
+}
+
+
+.post > div {
+	text-align: left;
+	font-style: normal;
+}
+
+.post  a {
+	color: #337ab7;
+}
+
+.post  p {
+	margin: 15px 0px 5px 0px;
+	line-height: 1.4;
+}
